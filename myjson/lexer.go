089dd67c1975dc351ae905bc8719f1b7a277b9ca
monokemonoke <kawabataryuhei@gmail.com>
2022-07-19 18:31:22 +0900

    🔥 Remove unused codes

--- myjson/lexer.go
+++ myjson/lexer.go
@@ -1,7 +1,5 @@
 package myjson
 
-import "log"
-
 type Lexer struct {
 	source string
 	curpos int
@@ -14,31 +12,3 @@ func NewLexer(source string) *Lexer {
 	return &lexer
 }
 
-func (l *Lexer) curChar() (string, bool) {
-	if l.curpos >= len(l.source) {
-		return "", false
-	}
-
-	res := l.source[l.curpos]
-	l.curpos++
-	return string(res), true
-}
-
-func (l *Lexer) Lex() []TOKEN_TYPE {
-	var tokens []TOKEN_TYPE
-
-	char, ok := l.curChar()
-	for ok {
-		log.Println(char)
-		char, ok = l.curChar()
-	}
-
-	tokens = []TOKEN_TYPE{
-		TOKEN_LEFT_BRACKET,
-		TOKEN_KEY,
-		TOKEN_STRING,
-		TOKEN_RIGHT_BRACKET,
-	}
-
-	return tokens
-}
