ac1fc62162c46cb5d4ede3ed1fa397252d55fa32
monokemonoke <kawabataryuhei@gmail.com>
2022-07-22 22:18:42 +0900

    🧪 parseSymbolについて失敗するテストを追加

--- myjson/parser_test.go
+++ myjson/parser_test.go
@@ -1,1 +1,30 @@
 package myjson
+
+import "testing"
+
+func TestParseSymbol(t *testing.T) {
+	cases := []struct {
+		input        string
+		expectSymbol string
+		expectErr    error
+	}{
+		{
+			input:        "symbol",
+			expectSymbol: "symbol",
+			expectErr:    nil,
+		},
+	}
+
+	for _, tc := range cases {
+		p := NewParser(tc.input)
+		actualSymbol, actualErr := p.parseSymbol()
+
+		if actualErr != tc.expectErr {
+			t.Errorf("want error `%s` but got `%s`\n", tc.expectErr, actualErr)
+		}
+
+		if actualSymbol != tc.expectSymbol {
+			t.Errorf("want symbol `%s` but got `%s`\n", tc.expectSymbol, actualSymbol)
+		}
+	}
+}
